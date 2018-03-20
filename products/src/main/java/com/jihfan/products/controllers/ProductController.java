4e913a17d6cb6cb8f4b11b3e34cd6464c5aa1e9e
JFLin <35427835+jlin728@users.noreply.github.com>
2018-03-20 14:19:45 -0400

    Update ProductController.java

--- products/src/main/java/com/jihfan/products/controllers/ProductController.java
+++ products/src/main/java/com/jihfan/products/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.jihfan.products.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -15,23 +16,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jihfan.products.models.Category;
 import com.jihfan.products.models.Product;
+import com.jihfan.products.repositories.ProductRepo;
 import com.jihfan.products.services.CatService;
 import com.jihfan.products.services.ProductService;
 
 @Controller
-@RequestMapping("/products")				//Reduces the need to repeat writing this
+@RequestMapping("/products")
 
 public class ProductController {
-	private ProductService pS;					//allows us to use all the CRUD methods that link to the DB
+	private ProductService pS;
 	private CatService cS;
 	
-	public ProductController(ProductService pS) {
+	public ProductController(ProductService pS, CatService cS) {
 		this.pS = pS;
+		this.cS = cS;
 	}
 // -------------------------------------------------------------------------------
 		@RequestMapping("/new")
 		public String product(Model model) {
-			model.addAttribute("product", new Product());
+			model.addAttribute("product", new Product());	
 			return "products";
 		}
 //		Alternate way to build a new product
@@ -39,48 +42,44 @@ public class ProductController {
 //			return "newProduct";
 //		}
 // --------------------------------------------------------------------------------
+
+	@PostMapping("/new")
+	public String create( @Valid @ModelAttribute("product") Product product, BindingResult res ) {
+		if(res.hasErrors()) {
+			return "products";
+		}		
+		pS.create(product);
+		return "redirect:/products/new";
+	}
 		
-		@PostMapping("/new")
-		public String create( @Valid @ModelAttribute("product") Product product, BindingResult res ) {
-			if(res.hasErrors()) {
-				return "products";
-			}
+		@RequestMapping("/{id}")
+		public String showProduct(@PathVariable("id") Long id, Model model) {			
+			Optional<Product> p = pS.find(id);
 			
-			pS.create(product);
-			return "redirect:/products/new";
-		}
-		
-		
-		
-//		public String allCats(Model model) {
-//			List<Category> categories = cS.allCat();			//two lines 
-//			model.addAttribute("categories", categories);		
-//			model.addAttribute("categories", cS.allCat());		//one line
-//			return "categories";
-//			}
-		
-		@RequestMapping ("{id}")
-		public String showProduct(@PathVariable("id") Long id, Model model) {
+			if(!p.isPresent()){
+				return "redirect:/products/new";
+			}
+			Product product = p.get();
+			
 			model.addAttribute("categories", cS.allCat());
-			model.addAttribute("product", pS.find(id));
+			model.addAttribute("product", product);
+			
 			return "showproduct";
 		}
 		
-		@PostMapping("{prod_id}/join")
+		@PostMapping("/{prod_id}/join")
 		public String join(@RequestParam("cat_id") Long cat_id, @PathVariable("prod_id") Long prod_id) {
 			
-		Product product = pS.find(prod_id);							// product you added to
-		List<Category> categories = product.getCategories();		// get all the categories that belong to the specific product
-		Category category = cS.find(cat_id);						// look for the category (drop down menu) that we selected
-		categories.add(category);									// add selected category to specific product's list of categories
-		product.setCategories(categories);							// set categories to include new selection
-		
-		pS.create(product);											// save
+		Product product = pS.find(prod_id).get();
+		List<Category> categories = product.getCategories();
+		Category category = cS.find(cat_id).get();
+		categories.add(category);
+		product.setCategories(categories);
+	
+		pS.create(product);									
 	
 		return "redirect:/products/"+prod_id;
 			
 		}
 		
-		
-
 }
