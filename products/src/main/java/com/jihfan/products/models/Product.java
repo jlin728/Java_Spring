610fdeb71c9af1fb694b6851707d13168edca477
JFLin <35427835+jlin728@users.noreply.github.com>
2018-03-20 14:24:55 -0400

    Update Product.java

--- products/src/main/java/com/jihfan/products/models/Product.java
+++ products/src/main/java/com/jihfan/products/models/Product.java
@@ -3,6 +3,7 @@ package com.jihfan.products.models;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -12,99 +13,88 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
-//import javax.persistence.Table;
-import javax.validation.constraints.Min;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-@Entity	
+@Entity
 //@Table(name = "product")
 
-public class Product {												
+public class Product {
 
 	// PRIVATE ATTRIBUTES
-	
-	@Id																// creates id
-	@GeneratedValue													// auto-generated
-	private long id;												
-	
-	@Size(min = 1, max = 255, message = "Name cannot be blank")		// 2) Validations
-	String name;													// 1) set attributes
-	
-	@Size(min = 3, max = 1024, message = "Please enter description")
-	String desc;
-	
-	@Min(0)														//@NotNull just means it cannot be blank.
+
+	@Id				
+	@GeneratedValue	
+
+	@Column
+	private long id;
+
+	@Column
+	@Size(min = 1, max = 255, message = "Name cannot be blank")
+	String name;
+
+	@Column
+	@Size(min = 3, max = 255, message = "Please enter description")
+	String description;
+
+	@Column
 	double price;
-	
+
+	@Column
+	@DateTimeFormat(pattern = "MM:dd:yyy HH:mm:ss")
+	private Date createdAt;
+
+	@Column
+	@DateTimeFormat(pattern = "MM:dd:yyy HH:mm:ss")
+	private Date updatedAt;
+
+	@PrePersist
+	public void onCreate() {this.createdAt = new Date();}
+
+	@PreUpdate
+	public void onUpdate() {this.updatedAt = new Date();}
+
 	// PUBLIC METHODS		Setters and Getters below
-	
-	public void setName (String name) {								// 3) Getters and Setters to access private attributes
+
+	public void setName (String name) {	
 		this.name = name;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
-	public void setDesc (String desc) {								
-		this.desc = desc;
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
-	
-	public String getDesc() {
-		return desc;
+
+	public String getDescription() {
+		return description;
 	}
-	
-	public void setPrice (double price) {							
+
+	public void setPrice (double price) {
 		this.price = price;
 	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
-	// ----------------------------------------------------------------------------------------------------
-	@ManyToMany(fetch = FetchType.LAZY)									// 5) MUST SET RELATIONSHIP HERE!!!
-	@JoinTable(
-			name = "products",
-			joinColumns = @JoinColumn(name = "product_id"),				// Hint - look at class. Self table
-			inverseJoinColumns = @JoinColumn(name = "category_id")		//		- this will the the other table you want to join at.
-	)
-	private List<Category> categories;
 
-	public void setCategories(List<Category> categories) {				// 6) Add setters/getters
-		this.categories= categories;
-	}
-	public List<Category> getCategories(){
-		return categories;
-	}
-	// ----------------------------------------------------------------------------------------------------
-	
-	@DateTimeFormat(pattern = "MM:dd:yyy HH:mm:ss")
-	private Date createdAt;
-	
-	@DateTimeFormat(pattern = "MM:dd:yyy HH:mm:ss")
-	private Date updatedAt;
-	
-	@PrePersist
-	public void onCreate() {this.createdAt = new Date();}
-	
-	@PreUpdate
-	public void onUpdate() {this.updatedAt = new Date();}
-	
 	public void setId(long id) {
 		this.id = id;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
-	
+
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
-	
+
 	public Date getCreatedAt() {
 		return createdAt;
 	}
@@ -112,23 +102,31 @@ public class Product {
 	public void setUpdatedAt(Date updatedAt) {
 		this.updatedAt = updatedAt;
 	}
-	
+
 		public Date getUpdatedAt() {
 			return updatedAt;
 	}
-	
-	public Product() {									// 4) Create blank constructor
+
+	// ----------------------------------------------------------------------------------------------------
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(
+			name = "products_categories",
+			joinColumns = @JoinColumn(name = "product_id"),
+			inverseJoinColumns = @JoinColumn(name = "category_id")		
+	)
+	private List<Category> categories;
+
+	public void setCategories(List<Category> categories) {
+		this.categories= categories;
+	}
+	public List<Category> getCategories(){
+		return categories;
+	}
+	// ----------------------------------------------------------------------------------------------------
+
+	public Product() {
 		this.createdAt = new Date ();
 		this.updatedAt = new Date ();
+
 	}
-	
-//	public Product (String name, String desc, double price) {
-//		super();
-//		this.name = name;
-//		this.desc = desc;
-//		this.price = price;
-//	}
-	
-	
-	
 }
